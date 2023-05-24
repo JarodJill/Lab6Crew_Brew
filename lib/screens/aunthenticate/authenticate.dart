c9d314f431e1105962487a2e1828243a51c7963c
JarodJill <jarod.jumin@yahoo.com>
2023-05-25 02:51:41 +0800

    first commit

---
+++ lib/screens/aunthenticate/authenticate.dart
@@ -0,0 +1,28 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:brew_crew/screens/aunthenticate/register.dart';
+import 'package:brew_crew/screens/aunthenticate/sign_in.dart';
+import 'package:flutter/material.dart';
+
+class Authenticate extends StatefulWidget {
+  const Authenticate({super.key});
+
+  @override
+  State<Authenticate> createState() => _AuthenticateState();
+}
+
+class _AuthenticateState extends State<Authenticate> {
+  bool showSignIn = true;
+  void toggleView(){
+    //print(showSignIn.toString());
+    setState(() => showSignIn = !showSignIn);
+  }
+  @override
+  Widget build(BuildContext context) {
+    if (showSignIn) {
+      return SignIn(toggleView:  toggleView);
+    } else {
+      return Register(toggleView:  toggleView);
+    }
+  }
+}
